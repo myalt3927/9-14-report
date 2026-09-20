fa872bce7aeada602444d56df8da0219c9a99e5a
alt <kwy713530@gmail.com>
2026-09-20 23:47:48 +0900

    Refactor printLine function and main logic

--- main.c
+++ main.c
@@ -1,32 +1,39 @@
 #include <stdio.h>
 
-void printLine(int spaces, int stars) {
-    for (int i = 0; i < spaces; i++) {
+void printLine(int spaces, int stars)
+{
+    for (int i = 0; i < spaces; i++)
+    {
         printf(" ");
     }
-    for (int i = 0; i < stars; i++) {
-        printf("*);
+    for (int i = 0; i < stars; i++)
+    {
+        printf("*");
     }
     printf("\n");
 }
 
-int main() {
+int main()
+{
     int n;
 
     scanf("%d", &n);
 
-    if (n < 3 || n % 2 == 0) {
+    if (n < 3 || n % 2 == 0)
+    {
         printf("3 이상의 홀수를 입력해야 합니다. \n");
         return 1;
     }
 
     int half = n / 2;
 
-    for (int i = 0; i <= half; i++) {
+    for (int i = 0; i <= half; i++)
+    {
         printLine(i, n - (2 * i));
     }
 
-    for (int i = half - 1; i >= 0; i--) {
+    for (int i = half - 1; i >= 0; i--)
+    {
         printLine(i, n - (2 * i));
     }
 
